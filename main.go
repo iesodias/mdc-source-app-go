5421d74a76d9952c0fb2359f097f1d19e0db2e9f
Ieso Dias <iesodias@gmail.com>
2023-06-23 18:44:24 -0300

    automated commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/mdc", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Mentoria Devops Cloud!")
+	})
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
